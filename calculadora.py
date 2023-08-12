706c687d8ea0644e9382a0902d34027c10e18c58
danielrivas29 <danielrivasm2005@gmail.com>
2023-08-12 12:00:59 -0500

    Operacion suma completada

---
+++ calculadora.py
@@ -0,0 +1,20 @@
+number1=int(input("Ingresa un numero"))
+number2=int(input("Ingresa otro numero"))
+
+eleccion = 0
+
+while eleccion !=6:
+    print("""
+    Indique la operacion a realizar:
+    
+    1)Suma
+    2)Resta
+    3)Multiplicacion
+    4)Division
+    5)Salir
+    """)
+    eleccion = int(input())
+
+    if eleccion == 1:
+        print(" ")
+        print("Resulatdo: ", number1,"+",number2,"=",number1+number2)
